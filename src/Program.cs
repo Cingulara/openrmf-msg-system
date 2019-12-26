40a0eef77f445aeeaf398fb74d669b9b75bb50f9
Dale Bingham <dalebingham@Dales-Main-Macbook.local>
2019-12-26 18:53:31 -0500

    adding the copyright notice

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Copyright (c) Cingulara 2019. All rights reserved.
+// Licensed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 license. See LICENSE file in the project root for full license information.
+using System;
 using System.Collections.Generic;
 using NATS.Client;
 using System.Text;
@@ -6,9 +8,7 @@ using NLog;
 using NLog.Config;
 using openrmf_msg_system.Models;
 using openrmf_msg_system.Data;
-using openrmf_msg_system.Classes;
 using MongoDB.Bson;
-using Newtonsoft.Json;
 
 namespace openrmf_msg_system
 {
